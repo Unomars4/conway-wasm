dd2ff87d84adb5ea0ef9402759ed2d72d3fe7062
Une Nyide <une.nyide@gmail.com>
2025-03-30 22:11:07 +0200

    add cell state enum

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,11 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern "C" {}
+
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
