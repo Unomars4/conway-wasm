e83c2b2b6b7e23b82a6953e451358d54e210616d
Une Nyide <une.nyide@gmail.com>
2025-03-30 22:29:23 +0200

    add live neighbour count method to universe struct

--- src/lib.rs
+++ src/lib.rs
@@ -24,4 +24,21 @@ impl Universe {
     pub fn get_index(&self, row: u32, column: u32) -> usize {
         (row * self.width + column) as usize
     }
+
+    pub fn live_neighbour_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        for change_in_row in [self.height - 1, 0, 1].iter().cloned() {
+            for change_in_col in [self.width - 1, 0, 1].iter().cloned() {
+                if change_in_row == 0 && change_in_col == 0 {
+                    continue;
+                }
+
+                let neighbour_row = (row + change_in_row) % self.height;
+                let neighbour_col = (column + change_in_col) % self.width;
+                let idx = self.get_index(neighbour_row, neighbour_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+        count
+    }
 }
