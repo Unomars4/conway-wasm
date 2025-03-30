3d3260ead427cc46c45690e04ec118f7d8f2687c
Une Nyide <une.nyide@gmail.com>
2025-03-30 23:05:30 +0200

    add tick method for universe struct

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,35 @@ pub struct Universe {
 }
 
 impl Universe {
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+
+        for row in 0..self.width {
+            for col in 0..self.height {
+                let idx = self.get_index(row, col);
+                let cell = self.cells[idx];
+                let live_neighbours = self.live_neighbour_count(row, col);
+
+                let next_cell = match (cell, live_neighbours) {
+                    // Death by Isolation
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    // Just right
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    // Death by Overpopulation/Competition
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    // New cell created by reproduction
+                    (Cell::Dead, 3) => Cell::Alive,
+                    // Stays the same
+                    (otherwise, _) => otherwise,
+                };
+
+                next[idx] = next_cell;
+            }
+        }
+
+        self.cells = next;
+    }
+
     fn get_index(&self, row: u32, column: u32) -> usize {
         (row * self.width + column) as usize
     }
